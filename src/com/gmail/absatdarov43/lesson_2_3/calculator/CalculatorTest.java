55163e0cf58749aa82c6aa8fe6a81fee294fe118
MaratAbs <absatdarov43@gmail.com>
2025-02-01 11:00:52 +0700

    новая структура файлов

---
+++ src/com/gmail/absatdarov43/lesson_2_3/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.gmail.absatdarov43.lesson_2_3.calculator;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner console = new Scanner(System.in);
+        String answer = "yes";
+
+        while (answer.equals("yes")) {
+            System.out.print("Введите первое число: ");
+            calculator.setFirstNumber(console.nextInt());
+
+            System.out.print("Введите знак арифметической операции (+, -, *, /, ^, %): ");
+            calculator.setOperation(console.next().charAt(0));
+
+            System.out.print("Введите второе число: ");
+            calculator.setSecondNumber(console.nextInt());
+
+            calculator.calculate();
+            console.nextLine();
+
+            do {
+                System.out.print("\nХотите продолжить вычисления? [yes/no]:  ");
+                answer = console.nextLine();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+        }
+        console.close();
+    }
+}
---
+++ src/com/gmail/absatdarov43/lesson_2_3/method/naming/Method.java
@@ -0,0 +1,8 @@
+package com.gmail.absatdarov43.lesson_2_3.method.naming;
+
+public class Method {
+    
+    public static String getCurrName() {
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
+    }
+}
