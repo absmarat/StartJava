7d4a864eb4a782dd1a3d557cb7114ff9665b7dae
MaratAbs <absatdarov43@gmail.com>
2025-02-24 15:25:52 +0700

    поместил в метод main вызов метода reverseArray(), переименовал метод enumerateArray()

--- src/com/github/absmarat/lesson_2_3_4/array/Reverser.java
+++ src/com/github/absmarat/lesson_2_3_4/array/Reverser.java
@@ -6,34 +6,38 @@ public class Reverser {
 
     public static void main(String[] args) {
         int[] numbers = {};
-        enumerateArray(numbers);
+        print(numbers);
+        reverseArray(numbers);
 
         int[] numbers1 = null;
-        enumerateArray(numbers1);
+        print(numbers1);
+        reverseArray(numbers1);
 
         int[] numbers2 = {6, 8, 9, 1};
-        enumerateArray(numbers2);
+        print(numbers2);
+        reverseArray(numbers2);
 
         int[] numbers3 = {13, 8, 5, 3, 2, 1, 1};
-        enumerateArray(numbers3);
+        print(numbers3);
+        reverseArray(numbers3);
     }
 
-    private static void enumerateArray(int[] array) {
-        if (array == null) {
-            System.out.println(array);
-            return;
-        }
+    private static void print(int[] array) {
         System.out.println("   До реверса: " + Arrays.toString(array));
-        reverseArray(array);
-        System.out.println("После реверса: " + Arrays.toString(array));
     }
 
     private static void reverseArray(int[] array) {
+        if (array == null) {
+            System.out.println("После реверса: " + array);
+            return;
+        }
         int len = array.length;
         for (int i = 0; i < len--; i++) {
             int temp = array[i];
             array[i] = array[len];
             array[len] = temp;
         }
+        System.out.println("После реверса: " + Arrays.toString(array));
+
     }
 }
