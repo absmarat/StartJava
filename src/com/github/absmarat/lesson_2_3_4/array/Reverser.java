31b96973526c7b0349f68dcecc3c7df5f83935d5
MaratAbs <absatdarov43@gmail.com>
2025-02-26 16:47:30 +0700

    в вызовы методов добавлены аргументы, в метод print() добавлен строковый параметр

--- src/com/github/absmarat/lesson_2_3_4/array/Reverser.java
+++ src/com/github/absmarat/lesson_2_3_4/array/Reverser.java
@@ -5,29 +5,32 @@ import java.util.Arrays;
 public class Reverser {
 
     public static void main(String[] args) {
+        String str1 = "   До реверса: ";
+        String str2 = "После реверса: ";
+
         int[] numbers = {};
-        print(numbers);
+        print(numbers, str1);
         reverseArray(numbers);
-        System.out.println("После реверса: " + Arrays.toString(numbers));
+        print(numbers, str2);
 
         int[] numbers1 = null;
-        print(numbers1);
+        print(numbers1, str1);
         reverseArray(numbers1);
-        System.out.println("После реверса: " + Arrays.toString(numbers1));
+        print(numbers1, str2);
 
         int[] numbers2 = {6, 8, 9, 1};
-        print(numbers2);
+        print(numbers2, str1);
         reverseArray(numbers2);
-        System.out.println("После реверса: " + Arrays.toString(numbers2));
+        print(numbers2, str2);
 
         int[] numbers3 = {13, 8, 5, 3, 2, 1, 1};
-        print(numbers3);
+        print(numbers3, str1);
         reverseArray(numbers3);
-        System.out.println("После реверса: " + Arrays.toString(numbers3));
+        print(numbers3, str2);
     }
 
-    private static void print(int[] array) {
-        System.out.println("   До реверса: " + Arrays.toString(array));
+    private static void print(int[] array, String str) {
+        System.out.println(str + Arrays.toString(array));
     }
 
     private static void reverseArray(int[] array) {
@@ -35,9 +38,9 @@ public class Reverser {
             return;
         }
         int len = array.length;
-        for (int i = 0; i < len--; i++) {
+        for (int i = 0; i < len; i++) {
             int temp = array[i];
-            array[i] = array[len];
+            array[i] = array[--len];
             array[len] = temp;
         }
     }
