6e05b4f26760e53ec2d5f60501347e86646ed2b8
MaratAbs <absatdarov43@gmail.com>
2025-03-26 12:13:17 +0700

    переименовал переменную, метод, исп. охранное выражение

--- src/com/github/absmarat/lesson_2_3_4/array/Factorial.java
+++ src/com/github/absmarat/lesson_2_3_4/array/Factorial.java
@@ -5,54 +5,54 @@ public class Factorial {
     public static void main(String[] args) {
         int[] numbers = {};
         long[] factorials = calculate(numbers);
-        print(factorials, numbers);
+        printExpr(factorials, numbers);
 
         int[] numbers1 = null;
         factorials = calculate(numbers1);
-        print(factorials, numbers1);
+        printExpr(factorials, numbers1);
 
         int[] numbers2 = {8, 0, 9};
         factorials = calculate(numbers2);
-        print(factorials, numbers2);
+        printExpr(factorials, numbers2);
 
         int[] numbers3 = {-3, 1, 7, 13};
         factorials = calculate(numbers3);
-        print(factorials, numbers3);
+        printExpr(factorials, numbers3);
 
         int[] numbers4 = {-22, -0};
         factorials = calculate(numbers4);
-        print(factorials, numbers4);
+        printExpr(factorials, numbers4);
     }
 
     private static long[] calculate(int... numbers) {
-        if (numbers != null) {
-            int length = numbers.length;
-            long[] result = new long[length];
-            for (int i = 0; i < length; i++) {
-                long factorial = 1;
-                for (int j = 1; j <= numbers[i]; j++) {
-                    factorial *= j;
-                }
-                result[i] = factorial;
-            }
-            return result;
+        if (numbers == null) {
+            return null;
         }
-        return null;
+
+        int length = numbers.length;
+        long[] factorials = new long[length];
+        for (int i = 0; i < length; i++) {
+            long factorial = 1;
+            for (int j = 2; j <= numbers[i]; j++) {
+                factorial *= j;
+            }
+            factorials[i] = factorial;
+        }
+        return factorials;
     }
 
-    private static void print(long[] factorials, int[] original) {
+    private static void printExpr(long[] factorials, int[] original) {
         if (original == null) {
             System.out.println("null");
             return;
         }
 
-        int length = original.length;
-        if (length == 0) {
+        if (original.length == 0) {
             System.out.println("Массив нулевой длины!");
             return;
         }
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < original.length; i++) {
             if (original[i] == 1 || original[i] == 0) {
                 System.out.println(original[i] + "! = 1");
                 continue;
@@ -63,13 +63,12 @@ public class Factorial {
                 continue;
             }
 
-            System.out.printf("%d", original[i]);
-            StringBuilder sb = new StringBuilder("! = ");
+            StringBuilder expr = new StringBuilder(original[i] + "! = ");
             for (int j = 1; j <= original[i]; j++) {
-                sb.append(j);
-                sb = (j < original[i]) ? sb.append(" * ") : sb.append(" = ");
+                expr.append(j);
+                expr = (j < original[i]) ? expr.append(" * ") : expr.append(" = ");
             }
-            System.out.println(sb.append(factorials[i]));
+            System.out.println(expr.append(factorials[i]));
         }
     }
 }
