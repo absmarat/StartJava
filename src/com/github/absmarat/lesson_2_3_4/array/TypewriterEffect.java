281481dbe7eb4ec3c5f939b24a68a17f059ffb16
MaratAbs <absatdarov43@gmail.com>
2025-05-07 08:33:43 +0700

    удалил дублирующий код

--- src/com/github/absmarat/lesson_2_3_4/array/TypewriterEffect.java
+++ src/com/github/absmarat/lesson_2_3_4/array/TypewriterEffect.java
@@ -1,27 +1,27 @@
 package com.github.absmarat.lesson_2_3_4.array;
 
-public class SubstringConversion {
+public class TypewriterEffect {
     public static void main(String[] args) throws InterruptedException {
-        String sentence = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
+        String enteredTxt = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
                 "- James Gosling\n";
-        sentence = convertToUpperCase(sentence);
-        print(sentence);
+        enteredTxt = toUpperCaseRange(enteredTxt);
+        type(enteredTxt);
 
-        sentence = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
+        enteredTxt = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
                 "- Robert Martin\n";
-        sentence = convertToUpperCase(sentence);
-        print(sentence);
+        enteredTxt = toUpperCaseRange(enteredTxt);
+        type(enteredTxt);
 
-        sentence = null;
-        convertToUpperCase(sentence);
-        print(sentence);
+        enteredTxt = null;
+        toUpperCaseRange(enteredTxt);
+        type(enteredTxt);
 
-        sentence = "";
-        sentence = convertToUpperCase(sentence);
-        print(sentence);
+        enteredTxt = "";
+        enteredTxt = toUpperCaseRange(enteredTxt);
+        type(enteredTxt);
     }
 
-    private static String convertToUpperCase(String sentence) {
+    private static String toUpperCaseRange(String sentence) {
         if (sentence == null) {
             return null;
         }
@@ -31,10 +31,10 @@ public class SubstringConversion {
         }
 
         // УДАЛЕНИЕ (ЗАМЕНА) ЗНАКОВ ПРЕПИНАНИЯ
-        String punctuationFreeSentence = sentence.replaceAll("\\p{P}", "");
+        String noPunctuation = sentence.replaceAll("[\\p{P}\\s]+", " ");
 
         // РАЗДЕЛЕНИЕ СТРОКИ НА МАССИВ ПОДСТРОК
-        String[] words = punctuationFreeSentence.split(" ");
+        String[] words = noPunctuation.split(" ");
 
         // ОПРЕДЕЛЕНИЕ САМОГО КОРОТКОГО И САМОГО ДЛИННОГО СЛОВА В СТРОКЕ И ИХ ИНДЕКСОВ
         String shortestWord = words[0];
@@ -56,14 +56,12 @@ public class SubstringConversion {
         String[] punctuationSentence = sentence.split(" ");
         StringBuilder modifiedSentence = new StringBuilder();
         length = punctuationSentence.length;
-        if (shortestWordIndex < longestWordIndex) {
-            for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
+            if (shortestWordIndex < longestWordIndex) {
                 modifiedSentence = ((i < shortestWordIndex) || (longestWordIndex < i))
                         ? modifiedSentence.append(punctuationSentence[i]).append(" ")
                         : modifiedSentence.append(punctuationSentence[i].toUpperCase()).append(" ");
-            }
-        } else {
-            for (int i = 0; i < length; i++) {
+            } else {
                 modifiedSentence = ((i < longestWordIndex) || (shortestWordIndex < i))
                         ? modifiedSentence.append(punctuationSentence[i]).append(" ")
                         : modifiedSentence.append(punctuationSentence[i].toUpperCase()).append(" ");
@@ -72,7 +70,7 @@ public class SubstringConversion {
         return modifiedSentence.toString();
     }
 
-    private static void print(String sentence) throws InterruptedException {
+    private static void type(String sentence) throws InterruptedException {
         if (sentence == null) {
             System.out.println("null");
             return;
