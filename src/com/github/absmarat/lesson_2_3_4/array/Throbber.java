39e2b4d330109846e1c9637e8ab272d9f15e3f94
MaratAbs <absatdarov43@gmail.com>
2025-05-17 10:25:21 +0700

    внёс коррективы

--- src/com/github/absmarat/lesson_2_3_4/array/Throbber.java
+++ src/com/github/absmarat/lesson_2_3_4/array/Throbber.java
@@ -2,15 +2,15 @@ package com.github.absmarat.lesson_2_3_4.array;
 
 import java.util.Random;
 
-public class LoadingAnimation {
+public class Throbber {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
 
     public static void main(String[] args) throws InterruptedException {
-        char[] chars = {'-', '\\', '|', '/'};
+        char[] animationElements = {'-', '\\', '|', '/'};
         int number = generateRandomNumber();
-        displayMessage(chars, number);
+        simulateHacking(animationElements, number);
     }
 
     private static int generateRandomNumber() {
@@ -18,19 +18,18 @@ public class LoadingAnimation {
         return rnd.nextInt(100);
     }
 
-    private static void displayMessage(char[] chars, int number) throws InterruptedException {
+    private static void simulateHacking(char[] animationElements, int number) throws InterruptedException {
         String msg = "Hacking: ";
 
         for (int i = 0; i < 3; i++) {
-            for (char ch : chars) {
-                System.out.print(msg + ch + "\r");
+            for (char element : animationElements) {
+                System.out.print(msg + element + "\r");
                 Thread.sleep(200);
             }
         }
 
-        String result = (number > 70)
-                ? (msg + ANSI_RED + "Access Granted!" + ANSI_RESET)
-                : (msg + ANSI_GREEN + "Access Denied!" + ANSI_RESET);
-        System.out.println(result);
+        System.out.print(msg);
+        String result = (number > 70) ? (ANSI_RED + "Access Granted!") : (ANSI_GREEN + "Access Denied!");
+        System.out.println(result + ANSI_RESET);
     }
 }
