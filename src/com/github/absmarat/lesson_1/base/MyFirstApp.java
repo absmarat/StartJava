450aa4344b73b5ac6958ddc9a728b5eb13664e9a
MaratAbs <absatdarov43@gmail.com>
2025-11-04 12:22:25 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_1/base/MyFirstApp.java
+++ src/com/github/absmarat/lesson_1/base/MyFirstApp.java
@@ -1,7 +1,6 @@
 package com.github.absmarat.lesson_1.base;
 
 public class MyFirstApp {
-    
     public static void main(String[] args) {
         System.out.println("Once write, run anywhere!");
     }
