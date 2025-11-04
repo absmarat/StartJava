3538954b29266cec9f054a8b92fa81d72c173aa3
MaratAbs <absatdarov43@gmail.com>
2025-11-04 12:52:38 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/method/naming/Method.java
+++ src/com/github/absmarat/lesson_2_3_4/method/naming/Method.java
@@ -1,7 +1,6 @@
 package com.github.absmarat.lesson_2_3_4.method.naming;
 
 public class Method {
-    
     public static String getCurrName() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
