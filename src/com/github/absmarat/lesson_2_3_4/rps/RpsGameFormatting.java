b8370e2b6a9cec34245edf2e38ac97b8b2337b3f
MaratAbs <absatdarov43@gmail.com>
2025-11-04 12:59:12 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/rps/RpsGameFormatting.java
+++ src/com/github/absmarat/lesson_2_3_4/rps/RpsGameFormatting.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RpsGameFormatting {
-
     private static final String ROCK = "R";
     private static final String SCISSORS = "S";
     private static final String PAPER = "P";
