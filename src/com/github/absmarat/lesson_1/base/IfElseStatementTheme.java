3054edb215939008ee5f3c64d093e622f9465e79
MaratAbs <absatdarov43@gmail.com>
2025-11-04 12:21:19 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_1/base/IfElseStatementTheme.java
+++ src/com/github/absmarat/lesson_1/base/IfElseStatementTheme.java
@@ -1,7 +1,6 @@
 package com.github.absmarat.lesson_1.base;
 
 public class IfElseStatementTheme {
-
     public static void main(String[] args) {
         // ЗАДАНИЕ 1
         System.out.println("1. Перевод псевдокода на язык Java\n");
