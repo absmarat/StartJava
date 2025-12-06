30a956ea46abba6fb6e8f68fababc19adf9f8c0b
MaratAbs <absatdarov43@gmail.com>
2025-12-06 12:21:41 +0700

    внес изменения

--- src/com/github/absmarat/lesson_2_3_4/array/HangmanGameMain.java
+++ src/com/github/absmarat/lesson_2_3_4/array/HangmanGameMain.java
@@ -1,4 +1,30 @@
 package com.github.absmarat.lesson_2_3_4.array;
 
+import java.util.Scanner;
+
 public class HangmanGameMain {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        HangmanGame game = new HangmanGame();
+        String answer = "yes";
+
+        while (answer.equals("yes")) {
+            game.start();
+            System.out.print("\nХотите продолжить игру? [yes/no]:  ");
+            answer = console.nextLine().toLowerCase();
+
+            if (!answer.equals("yes") && !answer.equals("no")) {
+                answer = enterCorrectAnswer(console, answer);
+            }
+        }
+        console.close();
+    }
+
+    private static String enterCorrectAnswer(Scanner console, String answer) {
+        while (!answer.equals("yes") && !answer.equals("no")) {
+            System.out.println("Введите корректный ответ [yes / no]: ");
+            answer = console.nextLine().toLowerCase();
+        }
+        return answer;
+    }
 }
