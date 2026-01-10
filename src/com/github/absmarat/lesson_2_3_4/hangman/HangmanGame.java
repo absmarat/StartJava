4c1ad86d888b0469bdd0b95fb7ce286b1baa5054
MaratAbs <absatdarov43@gmail.com>
2026-01-10 16:33:34 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/hangman/HangmanGame.java
+++ src/com/github/absmarat/lesson_2_3_4/hangman/HangmanGame.java
@@ -3,39 +3,39 @@ package com.github.absmarat.lesson_2_3_4.hangman;
 import java.util.Scanner;
 
 public class HangmanGame {
-    private String[] gallows;
+    private String[] words = {"ВИСЕЛИЦА", "АЛГОРИТМ", "КОМПЬЮТЕР", "ИГРА", "ПРОГРАММА"};
+    private String[] gallows = {
+            "_______",
+            "|     |",
+            "|     @",
+            "|    /|\\",
+            "|    / \\",
+            "| GAME OVER!"
+    };
     private int maxAttempts;
-    private String[] words;
-    private Scanner scanner;
-    private String selectedWord;
     private int currAttempt;
+
+    private String selectedWord;
+
+    private Scanner scanner;
     private StringBuilder wrongLetters;
     private StringBuilder mask;
 
     public HangmanGame() {
-        gallows = new String[] {
-                "_______",
-                "|     |",
-                "|     @",
-                "|    /|\\",
-                "|    / \\",
-                "| GAME OVER!"
-        };
         maxAttempts = gallows.length;
-        words = new String[] {"ВИСЕЛИЦА", "КОМПЬЮТЕР", "ИГРА", "АЛГОРИТМ", "ПРОГРАММИРОВАНИЕ"};
-        scanner = new Scanner(System.in);
-        selectedWord = words[(int) (Math.random() * words.length)];
         currAttempt = maxAttempts;
+        selectedWord = words[(int) (Math.random() * words.length)];
+        scanner = new Scanner(System.in);
         wrongLetters = new StringBuilder();
         mask = new StringBuilder("*".repeat(selectedWord.length()));
     }
 
     public void start() {
-        printWelcomeMessage(currAttempt);
+        printWelcomeMessage();
 
         while (!selectedWord.contentEquals(mask) && currAttempt > 0) {
-            displayGameState(mask, wrongLetters);
-            char guess = enterLetter(scanner);
+            displayGameState();
+            char guess = enterLetter();
 
             if (isReEnteredLetter(guess, mask)) {
                 System.out.println("Буква " + "\"" + guess + "\"" + " уже угадана!");
@@ -47,73 +47,75 @@ public class HangmanGame {
                 continue;
             }
 
-            boolean found = false;
-            for (int i = 0; i < selectedWord.length(); i++) {
-                if (guess == selectedWord.charAt(i)) {
-                    mask.setCharAt(i, guess);
-                    found = true;
-
-                    if (currAttempt < maxAttempts) {
-                        System.out.printf("""
-                                Буква "%s" верна! Количество попыток: %d
-                                 """, guess, ++currAttempt);
-                        displayHangmanElements(maxAttempts, currAttempt, gallows);
-                    }
-                }
-            }
-
-            if (!found) {
-                wrongLetters.append(guess).append(",");
-                System.out.printf("""
-                        Буквы "%s" нет в угадываемом слове! Количество попыток: %d
-                        """, guess, --currAttempt);
-                displayHangmanElements(maxAttempts, currAttempt, gallows);
-            }
-            printGameResult(mask, selectedWord, currAttempt);
+            processGuess(guess);
+            displayHangmanElements();
         }
+        printGameResult();
     }
 
-    private static void printWelcomeMessage(int currAttempt) {
+    private void printWelcomeMessage() {
         System.out.printf("""
+                
                 Добро пожаловать в игру!
                 У вас %d попыток, чтобы угадать слово.
                 """, currAttempt);
     }
 
-    private static void displayGameState(StringBuilder mask, StringBuilder wrongLetters) {
+    private void displayGameState() {
         System.out.println("Угадываемое слово: " + mask);
         System.out.println("Неверно введённые буквы: " + wrongLetters);
     }
 
-    private static char enterLetter(Scanner scanner) {
+    private char enterLetter() {
         char letter;
         while (true) {
-            System.out.print("Введите букву из кириллического алфавита: ");
+            System.out.print("\nВведите букву из кириллического алфавита: ");
             letter = scanner.next().toUpperCase().charAt(0);
 
-            if ((letter >= 'А' && letter <= 'Я') || letter == 'Ё') {
+            if (isCyrillicLetter(letter)) {
                 break;
-            } else {
-                System.out.printf("""
-                        Символ "%s" не корректен, введите букву кириллического алфавита!
-                        """, letter);
             }
+            System.out.println("Символ " + "\"" + letter + "\"" + " не корректен.");
         }
         return letter;
     }
 
-    private static boolean isReEnteredLetter(char letter, StringBuilder str) {
-        boolean found = false;
+    private boolean isCyrillicLetter(char letter) {
+        return (letter >= 'А' && letter <= 'Я') || letter == 'Ё';
+    }
 
+    private boolean isReEnteredLetter(char letter, StringBuilder str) {
         for (int i = 0; i < str.length(); i++) {
             if (letter == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void processGuess(char guess) {
+        boolean found = false;
+
+        for (int i = 0; i < selectedWord.length(); i++) {
+            if (guess == selectedWord.charAt(i)) {
+                mask.setCharAt(i, guess);
                 found = true;
             }
         }
-        return found;
+
+        if (found) {
+            if (currAttempt < maxAttempts) {
+                System.out.println("Буква " + "\"" + guess + "\" верна! " +
+                        "Количество попыток: " + (++currAttempt));
+            }
+        } else {
+            wrongLetters.append(guess).append(",");
+            System.out.println("Буквы " + "\"" + guess + "\"" + " нет в угадываемом слове!" +
+                    " Количество попыток: " + (--currAttempt));
+        }
     }
 
-    private static void displayHangmanElements(int maxAttempts, int currAttempt, String[] gallows) {
+    private void displayHangmanElements() {
         StringBuilder elements = new StringBuilder();
         int hangmanElementsAmount = maxAttempts - currAttempt;
 
@@ -123,16 +125,15 @@ public class HangmanGame {
         System.out.print(elements);
     }
 
-    private static void printGameResult(StringBuilder mask, String selectedWord, int currAttempt) {
-        System.out.println(selectedWord.contentEquals(mask) ? provideVictoryMessage(mask)
-                : currAttempt == 0 ? provideDefeatMessage(selectedWord) : "");
+    private void printGameResult() {
+        System.out.println(currAttempt > 0 ? provideVictoryMessage() : provideDefeatMessage());
     }
 
-    private static String provideVictoryMessage(StringBuilder mask) {
+    private String provideVictoryMessage() {
         return "Поздравляю! Вы угадали слово \"" + mask + "\"";
     }
 
-    private static String provideDefeatMessage(String selectedWord) {
+    private String provideDefeatMessage() {
         return "Все попытки исчерпаны. Вы проиграли! Загаданное слово \"" + selectedWord + "\"";
     }
 }
