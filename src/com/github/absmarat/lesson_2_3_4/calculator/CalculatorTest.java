73fcf7260ba04716a9e84fb95f05509340091403
MaratAbs <absatdarov43@gmail.com>
2026-01-19 20:14:48 +0700

    модифицировал код калькулятора

--- src/com/github/absmarat/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/github/absmarat/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,31 +1,45 @@
 package com.github.absmarat.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Scanner console = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         String answer = "yes";
 
-        while (answer.equals("yes")) {
-            System.out.print("Введите первое число: ");
-            calculator.setFirstNumber(console.nextInt());
+        while (!"no".equals(answer)) {
+            Calculator calculator = new Calculator();
 
-            System.out.print("Введите знак арифметической операции (+, -, *, /, ^, %): ");
-            calculator.setOperation(console.next().charAt(0));
-
-            System.out.print("Введите второе число: ");
-            calculator.setSecondNumber(console.nextInt());
-
-            calculator.calculate();
-            console.nextLine();
-
-            do {
+            if ("yes".equals(answer)) {
+                String expression = inputExpr(scan);
+                double result = calculator.calculate(expression);
+                printResult(expression, result);
                 System.out.print("\nХотите продолжить вычисления? [yes/no]:  ");
-                answer = console.nextLine();
-            } while (!answer.equals("yes") && !answer.equals("no"));
+            }
+
+            if (!"no".equals(answer) && !"yes".equals(answer)) {
+                System.out.print("Введите корректный ответ [yes / no]: ");
+            }
+            answer = scan.nextLine().toLowerCase().trim();
         }
-        console.close();
+    }
+
+    private static String inputExpr(Scanner scan) {
+        System.out.print("Введите выражение из трёх аргументов, например, 10 ^ 2: ");
+        return scan.nextLine().trim();
+    }
+
+    private static void printResult(String expression, double result) {
+        if (!Double.isNaN(result)) {
+            String strFormattedNum = formatResult(result);
+            System.out.println(expression + " = " + strFormattedNum);
+        }
+    }
+
+    private static String formatResult(double result) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.###");
+        String formattedNum = decimalFormat.format(result);
+        return formattedNum;
     }
 }
