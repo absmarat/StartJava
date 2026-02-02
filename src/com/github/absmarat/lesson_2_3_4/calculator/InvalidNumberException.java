9b3d22b9680e3cd6be02287f3c0f54b7f8227f86
MaratAbs <absatdarov43@gmail.com>
2026-02-02 23:21:40 +0700

    модифицированный калькулятор

--- src/com/github/absmarat/lesson_2_3_4/calculator/InvalidNumberException.java
+++ src/com/github/absmarat/lesson_2_3_4/calculator/InvalidNumberException.java
@@ -1,4 +1,7 @@
 package com.github.absmarat.lesson_2_3_4.calculator;
 
-public class InvalidNumberException {
+public class InvalidNumberException extends RuntimeException {
+    public InvalidNumberException(String message) {
+        super(message);
+    }
 }
