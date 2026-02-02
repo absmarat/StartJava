2a67df7556bf01707bc4113484fb23605a6c02eb
MaratAbs <absatdarov43@gmail.com>
2026-02-02 23:17:30 +0700

    модифицированный калькулятор

--- src/com/github/absmarat/lesson_2_3_4/calculator/Calculator.java
+++ src/com/github/absmarat/lesson_2_3_4/calculator/Calculator.java
@@ -3,47 +3,50 @@ package com.github.absmarat.lesson_2_3_4.calculator;
 public class Calculator {
     private static final int EXPRESSION_LENGTH = 3;
 
-    public double calculate(String expression) {
-        String[] expressionElements = expression.split(" ");
+    public static double calculate(String expression) {
+        String cleanedExcpression = expression.trim().replaceAll("\\s+", " ");
+        String[] expressionElements = cleanedExcpression.split(" ");
 
         if (expressionElements.length != EXPRESSION_LENGTH) {
-            System.out.println("Ошибка: выражение должно содержать три аргумента, например: 2 ^ 10");
-            return Double.NaN;
+            throw new InvalidExpressionException("""
+                    Ошибка: выражение должно содержать три аргумента, например: 2 ^ 10""");
         }
 
-        try {
-            int firstNumber = Integer.parseInt(expressionElements[0]);
-            char operation = expressionElements[1].charAt(0);
-            int secondNumber = Integer.parseInt(expressionElements[2]);
+        if (!expressionElements[0].matches("-?\\d+")) {
+            throw new InvalidNumberException("Ошибка: первый операнд не является целым числом.");
+        }
 
-            switch (operation) {
-                case '+':
-                    return firstNumber + secondNumber;
-                case '-':
-                    return firstNumber - secondNumber;
-                case '*':
-                    return firstNumber * secondNumber;
-                case '/':
-                case '%':
-                    if (secondNumber == 0) {
-                        System.out.println("Ошибка: деление на ноль запрещено!");
-                        return Double.NaN;
-                    }
-                    return divOrMod(firstNumber, secondNumber, operation);
-                case '^':
-                    return Math.pow(firstNumber, secondNumber);
-                default:
-                    System.out.println("Ошибка! Операция " + operation + " не поддерживается.");
-                    return Double.NaN;
+        if (!expressionElements[2].matches("-?\\d+")) {
+            throw new InvalidNumberException("Ошибка: второй операнд не является целым числом.");
+        }
+
+        String checkedOperator = expressionElements[1];
+        if (checkedOperator.length() != 1) {
+            throw new UnsupportedOperationException("Ошибка: " +
+                    "оператор \"" + checkedOperator + "\" не поддерживается." +
+                    " Допустимые операторы: +, -, *, /, %, ^");
+        }
+
+        int firstNumber = Integer.parseInt(expressionElements[0]);
+        char operation = expressionElements[1].charAt(0);
+        int secondNumber = Integer.parseInt(expressionElements[2]);
+
+        return switch (operation) {
+            case '+' -> firstNumber + secondNumber;
+            case '-' -> firstNumber - secondNumber;
+            case '*' -> firstNumber * secondNumber;
+            case '/', '%' -> {
+                if (secondNumber == 0) {
+                    throw new ArithmeticException("Ошибка: деление на ноль запрещено!");
+                }
+                // Является ли тернарный оператор после оператора yield оптимальной альтернативой
+                // вспомогательному методу divOrMod()?
+                yield (operation == '/') ? (double) firstNumber / secondNumber
+                        : Math.floorMod(firstNumber, secondNumber);
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: введён нечисловой символ.");
-            return Double.NaN;
-        }
-    }
-
-    private double divOrMod(int firstNumber, int secondNumber, char operation) {
-        return (operation == '/') ? (double) firstNumber / secondNumber
-                : Math.floorMod(firstNumber, secondNumber);
+            case '^' -> Math.pow(firstNumber, secondNumber);
+            default -> throw new UnsupportedOperationException("Ошибка: " +
+                    "операция " + operation + " не поддерживается.");
+        };
     }
 }
