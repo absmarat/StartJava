6a9cde32d532df7a88ed94e75f7d7d80195635f5
MaratAbs <absatdarov43@gmail.com>
2026-02-04 20:20:41 +0700

    замена класса

---
+++ src/com/github/absmarat/lesson_2_3_4/calculator/InvalidExpressionLengthExeption.java
@@ -0,0 +1,7 @@
+package com.github.absmarat.lesson_2_3_4.calculator;
+
+public class InvalidExpressionLengthExeption extends RuntimeException {
+    public InvalidExpressionLengthExeption(String message) {
+        super(message);
+    }
+}
