d27aca9078bc8ddf1886ff343e52f7cd1fa7acf4
MaratAbs <absatdarov43@gmail.com>
2026-02-04 21:53:45 +0700

    исправил опечатку в слове Exception

--- src/com/github/absmarat/lesson_2_3_4/calculator/Calculator.java
+++ src/com/github/absmarat/lesson_2_3_4/calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
         String[] expressionElements = expression.split(" ");
 
         if (expressionElements.length != EXPRESSION_LENGTH) {
-            throw new InvalidExpressionLengthExeption(
+            throw new InvalidExpressionLengthException(
                     "Ошибка: выражение должно содержать три аргумента, например: 2 ^ 10");
         }
 
