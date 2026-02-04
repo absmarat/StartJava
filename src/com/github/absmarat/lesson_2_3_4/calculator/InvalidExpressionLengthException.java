1be08c4779ee77fe6ca8038d2b73e27cf78824d7
absmarat <140299270+absmarat@users.noreply.github.com>
2026-02-04 19:39:53 +0700

    Delete src/com/github/absmarat/lesson_2_3_4/calculator/InvalidExpressionLengthException.java
    удалил файл

--- src/com/github/absmarat/lesson_2_3_4/calculator/InvalidExpressionLengthException.java
+++
@@ -1,7 +0,0 @@
-package com.github.absmarat.lesson_2_3_4.calculator;
-
-public class InvalidExpressionException extends RuntimeException {
-    public InvalidExpressionException(String message) {
-        super(message);
-    }
-}
