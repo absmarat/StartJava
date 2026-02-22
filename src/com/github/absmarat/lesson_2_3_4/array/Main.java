8bff40bb603ef1c9a74ddee93fe4fd3685b42ada
MaratAbs <absatdarov43@gmail.com>
2026-02-22 14:39:57 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/array/Main.java
+++ src/com/github/absmarat/lesson_2_3_4/array/Main.java
@@ -3,7 +3,7 @@ package com.github.absmarat.lesson_2_3_4.array;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         reverseArray();
-        simulateHacking();
+        simulatePasswordHacking();
         calculateFactorial();
         fillArrayFloatNumbers();
         createTriangle();
@@ -13,65 +13,55 @@ public class Main {
     private static void reverseArray() {
         String beforeMsg = "   До реверса: ";
         String afterMsg = "После реверса: ";
+        int[][] initialData = {
+                {},
+                null,
+                {6, 8, 9, 1},
+                {13, 8, 5, 3, 2, 1, 1}
+        };
 
-        int[] numbers = {};
-        Console.printNumbers(numbers, beforeMsg);
-        Arrays.reverse(numbers);
-        Console.printNumbers(numbers, afterMsg);
-
-        Console.printNumbers(null, beforeMsg);
-        Arrays.reverse(null);
-        Console.printNumbers(null, afterMsg);
-
-        int[] numbers2 = {6, 8, 9, 1};
-        Console.printNumbers(numbers2, beforeMsg);
-        Arrays.reverse(numbers2);
-        Console.printNumbers(numbers2, afterMsg);
-
-        int[] numbers3 = {13, 8, 5, 3, 2, 1, 1};
-        Console.printNumbers(numbers3, beforeMsg);
-        Arrays.reverse(numbers3);
-        Console.printNumbers(numbers3, afterMsg);
+        for (int[] numbers : initialData) {
+            Console.printNumbers(numbers, beforeMsg);
+            int[] reversed = (numbers == null) ? null : Arrays.reverse(numbers);
+            Console.printNumbers(reversed, afterMsg);
+        }
     }
 
-    private static void simulateHacking() throws InterruptedException {
-        String result = Arrays.hack();
-        Console.printHackResult(result);
+    private static void simulatePasswordHacking() throws InterruptedException {
+        String[] ranges = {"48,57", "65,90", "97,122", "65,90", "33,47", "58,64", "123,126"};
+        char[] generatedPassword = Arrays.generateRandomSymbols(ranges);
+        Console.displayLoading(generatedPassword);
     }
 
     private static void calculateFactorial() {
-        int[] numbers = {};
-        long[] factorials = Arrays.calFctorial(numbers);
-        Console.printExpr(factorials, numbers);
+        int[][] initialData = {
+                {},
+                null,
+                {8, 0, 9},
+                {-3, 1, 7, 13},
+                {-22, -0}
+        };
 
-        int[] numbers1 = null;
-        factorials = Arrays.calFctorial(numbers1);
-        Console.printExpr(factorials, numbers1);
-
-        int[] numbers2 = {8, 0, 9};
-        factorials = Arrays.calFctorial(numbers2);
-        Console.printExpr(factorials, numbers2);
-
-        int[] numbers3 = {-3, 1, 7, 13};
-        factorials = Arrays.calFctorial(numbers3);
-        Console.printExpr(factorials, numbers3);
-
-        int[] numbers4 = {-22, -0};
-        factorials = Arrays.calFctorial(numbers4);
-        Console.printExpr(factorials, numbers4);
+        for (int[] numbers : initialData) {
+            long[] facrorials = (numbers == null) ? null : Arrays.calFctorial(numbers);
+            Console.printExpr(facrorials, numbers);
+        }
     }
 
     private static void fillArrayFloatNumbers() {
         int length = 15;
         float[] floatNumbers = new float[length];
         int[] ints = {-1, 15, 0, 14};
+        float[] filled;
         float[] modified;
         for (int index : ints) {
             if (index < 0 || index == length) {
                 Console.printErrorMessage(index);
                 continue;
             }
-            modified = Arrays.fillArray(floatNumbers, index);
+            filled = Arrays.fillArray(floatNumbers, length);
+            modified = Arrays.zeroOutSomeValues(filled, index, length);
+
             int count = 0;
             for (int i = 0; i < length; i++) {
                 if (modified[i] != floatNumbers[i]) {
@@ -83,33 +73,34 @@ public class Main {
     }
 
     private static void createTriangle() {
-        String triangle = Arrays.sortSymbols('0', '9', false);
-        Console.printString(triangle);
+        boolean[] sortFlags = {true, true, false};
+        char[][] initialData = {
+                {'0', '9'},
+                {'i', '/'},
+                {'A', 'J'}
+        };
+        int index = 0;
 
-        triangle = Arrays.sortSymbols('i', '/', true);
-        Console.printString(triangle);
-
-        triangle = Arrays.sortSymbols('A', 'J', true);
-        Console.printString(triangle);
+        for (char[] symbols : initialData) {
+            char startSymbol = symbols[0];
+            char endSymbol = symbols[1];
+            String triangle = Arrays.sortSymbols(startSymbol, endSymbol, sortFlags[index++]);
+            Console.printString(triangle);
+        }
     }
 
     private static void createTypewriterEffect() throws InterruptedException {
-        String enteredText = "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
-                "- James Gosling\n";
-        String[] modifiedText = Arrays.convertSubstringToUpperCase(enteredText);
-        Console.displayTypewriterEffect(modifiedText);
+        String[] quotes = {"Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
+                "- James Gosling\n",
+                "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
+                        "- Robert Martin\n",
+                null,
+                " "};
 
-        enteredText = "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
-                "- Robert Martin\n";
-        modifiedText = Arrays.convertSubstringToUpperCase(enteredText);
-        Console.displayTypewriterEffect(modifiedText);
-
-        enteredText = null;
-        modifiedText = Arrays.convertSubstringToUpperCase(enteredText);
-        Console.displayTypewriterEffect(modifiedText);
-
-        enteredText = "";
-        modifiedText = Arrays.convertSubstringToUpperCase(enteredText);
-        Console.displayTypewriterEffect(modifiedText);
+        for (String quote : quotes) {
+            String[] modifiedText = (quote == null || quote.isBlank()) ? null
+                    : Arrays.toUpperCaseRange(quote);
+            Console.displayTypewriterEffect(modifiedText);
+        }
     }
 }
