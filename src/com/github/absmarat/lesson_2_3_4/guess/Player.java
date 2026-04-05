edc856e5e6b5e8437589a507a66bf948385c520d
MaratAbs <absatdarov43@gmail.com>
2026-04-05 17:54:34 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/guess/Player.java
+++ src/com/github/absmarat/lesson_2_3_4/guess/Player.java
@@ -1,22 +1,42 @@
 package com.github.absmarat.lesson_2_3_4.guess;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    private int number;
+    private int usedAttempts;
+    private int[] incorrectNumbers;
 
     Player(String name) {
         this.name = name;
+        this.incorrectNumbers = new int[GuessNumber.MAX_ATTEMPTS];
+        this.usedAttempts = 0;
+        Arrays.fill(incorrectNumbers, 0);
     }
 
     public String getName() {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public void setNumber(int number) {
+        if (number < GuessNumber.MIN_NUMBER || number > GuessNumber.MAX_NUMBER) {
+            throw new IllegalArgumentException("Число должно быть в диапазоне от " +
+                    GuessNumber.MIN_NUMBER + " до " + GuessNumber.MAX_NUMBER + ".");
+        }
+        incorrectNumbers[usedAttempts] = number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void incrementAttempts() {
+        usedAttempts++;
+    }
+
+    public void reset() {
+        usedAttempts = 0;
+        Arrays.fill(incorrectNumbers, 0);
+    }
+
+    public String buildIncorrectGuessesString() {
+        int[] currentNumbers = Arrays.copyOf(incorrectNumbers, usedAttempts);
+        return Arrays.toString(currentNumbers).replaceAll("[\\[\\]]", "");
     }
 }
