ae521ce3671897e3fc1ed13700436e5ecdbd1525
MaratAbs <absatdarov43@gmail.com>
2026-04-12 10:59:54 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/github/absmarat/lesson_2_3_4/guess/GuessNumberTest.java
@@ -19,7 +19,7 @@ public class GuessNumberTest {
 
             if ("yes".equals(answer)) {
                 game.start();
-                System.out.print("Хотите продолжить игру? [yes/no]: ");
+                System.out.print("\nХотите продолжить игру? [yes/no]: ");
             }
 
             if (!"yes".equals(answer) && !"no".equals(answer)) {
