094dc14b63497b44ef5263cbd90e3fd2466ef3d3
MaratAbs <absatdarov43@gmail.com>
2026-04-12 10:59:01 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/guess/Player.java
+++ src/com/github/absmarat/lesson_2_3_4/guess/Player.java
@@ -1,42 +1,41 @@
 package com.github.absmarat.lesson_2_3_4.guess;
 
+import static com.github.absmarat.lesson_2_3_4.guess.GuessNumber.MAX_ATTEMPTS;
+import static com.github.absmarat.lesson_2_3_4.guess.GuessNumber.MAX_NUMBER;
+import static com.github.absmarat.lesson_2_3_4.guess.GuessNumber.MIN_NUMBER;
+
 import java.util.Arrays;
 
 public class Player {
     private String name;
-    private int usedAttempts;
+    private int attempts;
     private int[] guesses;
 
     Player(String name) {
         this.name = name;
-        this.guesses = new int[GuessNumber.MAX_ATTEMPTS];
-        this.usedAttempts = 0;
-        Arrays.fill(guesses, 0);
+        guesses = new int[MAX_ATTEMPTS];
     }
 
     public String getName() {
         return name;
     }
 
-    public void setNumber(int number) {
-        if (number < GuessNumber.MIN_NUMBER || number > GuessNumber.MAX_NUMBER) {
-            throw new IllegalArgumentException("Число должно быть в диапазоне от " +
-                    GuessNumber.MIN_NUMBER + " до " + GuessNumber.MAX_NUMBER + ". ");
-        }
-        guesses[usedAttempts] = number;
-    }
-
-    public void incrementUsedAttempt() {
-        usedAttempts++;
-    }
-
     public void reset() {
-        usedAttempts = 0;
+        attempts = 0;
         Arrays.fill(guesses, 0);
     }
 
-    public String buildGuessesString() {
-        int[] currentNumbers = Arrays.copyOf(guesses, usedAttempts);
-        return Arrays.toString(currentNumbers).replaceAll("[\\[\\]]", "");
+    public void addNumber(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("Число должно быть в диапазоне от " +
+                    MIN_NUMBER + " до " + MAX_NUMBER + ". ");
+        }
+        guesses[attempts] = number;
+        attempts++;
+    }
+
+    public int[] receiveGuesses() {
+        int[] currentNumbers = Arrays.copyOf(guesses, attempts);
+        return currentNumbers;
     }
 }
