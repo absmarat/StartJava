4030acde6c02f6df4298d516503d4b69a259c1fa
MaratAbs <absatdarov43@gmail.com>
2026-05-03 17:06:21 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/guess/Player.java
+++ src/com/github/absmarat/lesson_2_3_4/guess/Player.java
@@ -10,6 +10,8 @@ public class Player {
     private String name;
     private int attempts;
     private int[] guesses;
+    private int guess;
+    private int wins;
 
     Player(String name) {
         this.name = name;
@@ -20,18 +22,43 @@ public class Player {
         return name;
     }
 
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        if (guess < MIN_NUMBER || guess > MAX_NUMBER) {
+            throw new IllegalArgumentException("Число должно быть в диапазоне от " +
+                    MIN_NUMBER + " до " + MAX_NUMBER + ".");
+        }
+        this.guess = guess;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public void resetPlayerWins() {
+        wins = 0;
+    }
+
+    public void recordGuess(int guess) {
+        guesses[attempts] = guess;
+        attempts++;
+    }
+
     public void reset() {
         Arrays.fill(guesses, 0, attempts, 0);
         attempts = 0;
-    }
-
-    public void addNumber(int number) {
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
-            throw new IllegalArgumentException("Число должно быть в диапазоне от " +
-                    MIN_NUMBER + " до " + MAX_NUMBER + ". ");
-        }
-        guesses[attempts] = number;
-        attempts++;
+        guess = 0;
     }
 
     public int[] receiveGuesses() {
