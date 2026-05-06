bac188a57896a4ea72c305e8dd139ac806929b2d
MaratAbs <absatdarov43@gmail.com>
2026-05-06 20:06:40 +0700

    внесены изменения

--- src/com/github/absmarat/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/github/absmarat/lesson_2_3_4/guess/GuessNumberTest.java
@@ -3,23 +3,15 @@ package com.github.absmarat.lesson_2_3_4.guess;
 import java.util.Scanner;
 
 public class GuessNumberTest {
+    private static final int PLAYERS = 3;
+
     public static void main(String[] args) throws InterruptedException {
         Scanner console = new Scanner(System.in);
-        System.out.print("Введите имя первого игрока: ");
-        String firstName = console.nextLine();
-        Player p1 = new Player(firstName);
-
-        System.out.print("Введите имя второго игрока: ");
-        String secondName = console.nextLine();
-        Player p2 = new Player(secondName);
-
-        System.out.print("Введите имя третьего игрока: ");
-        String thirdName = console.nextLine();
-        Player p3 = new Player(thirdName);
+        Player[] players = addPlayers(console);
         String answer = "yes";
 
         while (!"no".equals(answer)) {
-            GuessNumber game = new GuessNumber(p1, p2, p3);
+            GuessNumber game = new GuessNumber(players);
 
             if ("yes".equals(answer)) {
                 game.start();
@@ -31,6 +23,16 @@ public class GuessNumberTest {
             answer = console.nextLine().toLowerCase().trim();
         }
         console.close();
+    }
+
+    private static Player[] addPlayers(Scanner console) {
+        Player[] players = new Player[PLAYERS];
+        for (int i = 0; i < PLAYERS; i++) {
+            System.out.print("Введите имя игрока №" + (i + 1) + ": ");
+            String name = console.nextLine();
+            players[i] = new Player(name);
+        }
+        return players;
     }
 }
 
