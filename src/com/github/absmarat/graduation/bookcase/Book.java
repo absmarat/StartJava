0559a5da074398a68845507ee9fd6f4ebc064561
MaratAbs <absatdarov43@gmail.com>
2026-05-30 10:57:31 +0700

    внесены изменения

--- src/com/github/absmarat/graduation/bookcase/Book.java
+++ src/com/github/absmarat/graduation/bookcase/Book.java
@@ -1,4 +1,26 @@
 package com.github.absmarat.graduation.bookcase;
 
+import java.time.Year;
+
 public class Book {
+    private String title;
+    private String author;
+    private Year year;
+
+
+    public Book(String title, String author, Year year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + title + "\", " + author + ", " + year;
+
+    }
 }
