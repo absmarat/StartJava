881a6b634fdcecb36149462f33411f4d0229cb8f
MaratAbs <absatdarov43@gmail.com>
2026-06-27 15:09:24 +0700

    внесены изменения

--- src/com/github/absmarat/graduation/bookcase/MenuItem.java
+++ src/com/github/absmarat/graduation/bookcase/MenuItem.java
@@ -1,4 +1,20 @@
 package com.github.absmarat.graduation.bookcase;
 
 public enum MenuItem {
+    ADD("Добавить книгу"),
+    FIND("Найти книгу"),
+    REMOVE("Удалить книгу"),
+    CLEAR("Удалить все книги"),
+    EXIT("Завершить работу программы");
+
+    private final String label;
+    static final int MIN_ID = 1;
+
+    MenuItem(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
