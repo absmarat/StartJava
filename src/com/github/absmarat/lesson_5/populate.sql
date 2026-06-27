fc52c11d9c0fddbfef943ef0e7a0183f80e7059f
MaratAbs <absatdarov43@gmail.com>
2026-06-27 18:38:19 +0700

    скорректировал формат даты

--- src/com/github/absmarat/lesson_5/populate.sql
+++ src/com/github/absmarat/lesson_5/populate.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill)
-VALUES ('Striker Eureka',  5, 76, 1.850, 'Destroyed', 'Australia', '2020-07-06', 17),
+VALUES ('Strike Eureka',  5, 76, 1.850, 'Destroyed', 'Australia', '2020-07-06', 17),
        ('Cherno Alpha',   1, 79, 2.412, 'Destroyed', 'Russia',    '2015-07-06', 9),
        ('Crimson Typhoon',  4, 75, 1.722, 'Destroyed', 'China', '2019-02-06', 8),
        ('Coyote Tango',   1, 86, 2.312, 'Decommissioned', 'Japan',    '2014-04-06', 11),
@@ -9,4 +9,4 @@ VALUES ('Striker Eureka',  5, 76, 1.850, 'Destroyed', 'Australia', '2020-07-06',
        ('Vulcan Specter',   6, 74, 1.780, 'Destroyed', 'USA',    '2023-05-06', 2),
        ('Obsidian Fury',   6, 80, 2.100, 'Active', 'USA', '2025-05-06', 4),
        ('Titan Redeemer',   7, 82, 2.200, 'Active', 'Germany',    '2014-03-01', 3),
-       ('Atlas Psycho', 1, 76, 1.546, 'Destroyed', 'Japan', '2015.01.01', 17);
+       ('Atlas Psycho', 1, 76, 1.546, 'Destroyed', 'Japan', '2015-01-01', 17);
