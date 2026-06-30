835ee7683d6a4e6b81011bec9a6a26c0f57f6bc2
MaratAbs <absatdarov43@gmail.com>
2026-06-30 14:41:58 +0700

    внёс коррективы

--- src/com/github/absmarat/lesson_5/populate.sql
+++ src/com/github/absmarat/lesson_5/populate.sql
@@ -10,10 +10,10 @@ INSERT INTO jaegers (
     kaiju_kill
 )
 VALUES 
-            ('Strike Eureka',        5,  76,  1.850,  'Destroyed',          'Australia',      '2020-07-06',  17),
+            ('Strike Eureka',       5,  76,  1.850,  'Destroyed',          'Australia',      '2020-07-06',  17),
             ('Cherno Alpha',       1,  79,  2.412,  'Destroyed',          'Russia',          '2015-07-06',   9),
-            ('Crimson Typhoon',  4,  75,  1.722,  'Destroyed',          'China',           '2019-02-06',   8),
-            ('Coyote Tango',       1,  86,  2.312,  'Decommissioned', 'Japan',           '2014-04-06',  11),
+            ('Crimson Typhoon', 4,  75,  1.722,  'Destroyed',          'China',           '2019-02-06',   8),
+            ('Coyote Tango',      1,  86,  2.312,  'Decommissioned', 'Japan',           '2014-04-06',  11),
             ('Echo Sabre',          6,  69,  1.650,  'Active',                'Canada',        '2024-03-06',  11),
             ('Saber Athena',       5,  72,  1.550,  'Active',                'South Korea',  '2021-03-06',   5),
             ('Vulcan Specter',     6,  74,  1.780,  'Destroyed',           'USA',            '2023-05-06',   2),
