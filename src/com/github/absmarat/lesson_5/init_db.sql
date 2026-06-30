69abafeb2be079d20fb0962f1667c4fa67c41215
MaratAbs <absatdarov43@gmail.com>
2026-06-30 14:41:13 +0700

    внёс коррективы

--- src/com/github/absmarat/lesson_5/init_db.sql
+++ src/com/github/absmarat/lesson_5/init_db.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS jaegers;
 
 CREATE TABLE jaegers (
     id                 SERIAL PRIMARY KEY,
-    model_name  VARCHAR(30)  NOT NULL,
-    mark            SMALLINT      NOT NULL,
+    model_name  VARCHAR(30) NOT NULL,
+    mark            SMALLINT       NOT NULL,
     height           NUMERIC       NOT NULL,
     weight           NUMERIC       NOT NULL,
     status            VARCHAR(15) NOT NULL,
-    origin             VARCHAR(30) NOT NULL,
-    launch            DATE            NOT NULL,
-    kaiju_kill         SMALLINT      NOT NULL
+    origin            VARCHAR(30) NOT NULL,
+    launch           DATE             NOT NULL,
+    kaiju_kill        SMALLINT      NOT NULL
 );
 
 \ir 'populate.sql'
