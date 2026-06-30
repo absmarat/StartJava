d2ceeaa55d4537051bbf865b01c5bbb391da04d0
MaratAbs <absatdarov43@gmail.com>
2026-06-30 17:46:35 +0700

    внёсены коррективы

--- src/com/github/absmarat/lesson_5/populate.sql
+++ src/com/github/absmarat/lesson_5/populate.sql
@@ -10,13 +10,13 @@ INSERT INTO jaegers (
     kaiju_kill
 )
 VALUES 
-            ('Strike Eureka',       5,  76,  1.850,  'Destroyed',          'Australia',      '2020-07-06',  17),
-            ('Cherno Alpha',       1,  79,  2.412,  'Destroyed',          'Russia',          '2015-07-06',   9),
-            ('Crimson Typhoon', 4,  75,  1.722,  'Destroyed',          'China',           '2019-02-06',   8),
-            ('Coyote Tango',      1,  86,  2.312,  'Decommissioned', 'Japan',           '2014-04-06',  11),
-            ('Echo Sabre',          6,  69,  1.650,  'Active',                'Canada',        '2024-03-06',  11),
-            ('Saber Athena',       5,  72,  1.550,  'Active',                'South Korea',  '2021-03-06',   5),
-            ('Vulcan Specter',     6,  74,  1.780,  'Destroyed',           'USA',            '2023-05-06',   2),
-            ('Obsidian Fury',       6,  80,  2.100,  'Active',                'USA',            '2025-05-06',   4),
-            ('Titan Redeemer',    7,  82,  2.200,  'Active',                'Germany',     '2014-03-01',    3),
-            ('Atlas Psycho',        1,  76,  1.546,  'Destroyed',           'Japan',          '2015-01-01',  17);
+    ('Strike Eureka',   5,  76,  1.850,  'Destroyed',      'Australia',   '2020-07-06', 17),
+    ('Cherno Alpha',    1,  79,  2.412,  'Destroyed',      'Russia',      '2015-07-06',  9),
+    ('Crimson Typhoon', 4,  75,  1.722,  'Destroyed',      'China',       '2019-02-06',  8),
+    ('Coyote Tango',    1,  86,  2.312,  'Decommissioned', 'Japan',       '2014-04-06', 11),
+    ('Echo Sabre',      6,  69,  1.650,  'Active',         'Canada',      '2024-03-06', 11),
+    ('Saber Athena',    5,  72,  1.550,  'Active',         'South Korea', '2021-03-06',  5),
+    ('Vulcan Specter',  6,  74,  1.780,  'Destroyed',      'USA',         '2023-05-06',  2),
+    ('Obsidian Fury',   6,  80,  2.100,  'Active',         'USA',         '2025-05-06',  4),
+    ('Titan Redeemer',  7,  82,  2.200,  'Active',         'Germany',     '2014-03-01',  3),
+    ('Atlas Psycho',    1,  76,  1.546,  'Destroyed',      'Japan',       '2015-01-01', 17);
